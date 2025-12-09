11ee90a65e96dec65a9559ca5a9549ba3fc9b8cb
Cactus <88003472+Purple-Palm@users.noreply.github.com>
2025-12-08 18:06:31 -0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,8 @@
 @echo off
+Title GTA Online Glitcher Installer and Runner
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
+    echo [INFO] Requesting administrative privileges...
     goto UACPrompt
 ) else ( goto gotAdmin )
 :UACPrompt
@@ -14,9 +15,43 @@ if '%errorlevel%' NEQ '0' (
 :gotAdmin
     pushd "%CD%"
     CD /D "%~dp0"
-    echo Activating Virtual Environment...
+python --version >nul 2>&1
+if %errorlevel% NEQ 0 (
+    echo [CRITICAL ERROR] Python is not installed or not added to PATH.
+    echo Please install Python 3.10+ and check "Add to PATH" during installation.
+    pause
+    exit
+)
+if exist ".venv" (
+    echo [INFO] Virtual environment found. Starting...
+    goto StartScript
+) else (
+    echo [INIT] First run detected. Setting up environment...
+    echo [1/3] Creating .venv...
+    python -m venv .venv
     call .venv\Scripts\activate
-    echo Starting Python Script...
+    python -m pip install --upgrade pip
+    if not exist "requirements.txt" (
+        echo [2/3] Genererating requirements.txt...
+        (
+            echo keyboard
+            echo mss
+            echo numpy
+            echo opencv-python
+            echo pyautogui
+            echo pydirectinput
+        ) > requirements.txt
+    )
+    echo [3/3] Installing dependencies...
+    pip install -r requirements.txt
+    echo [SUCCESS] Setup complete!
+    goto StartScript
+)
+:StartScript
+    if not defined VIRTUAL_ENV call .venv\Scripts\activate
+    echo.
+    echo Launching Main Script...
     python main.py
-
+    echo.
+    echo [EXIT] Script finished.
     pause
